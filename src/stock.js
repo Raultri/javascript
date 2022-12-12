e990cf4b723139220fb7c25b4c2a23f492d2de80
Raultri <111189223+Raultri@users.noreply.github.com>
2022-12-12 12:23:59 +0100

    Add files via upload

---
+++ src/stock.js
@@ -0,0 +1,70 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Specialized Aethos",
+        tipo: "bicicleta",
+        desc: "una bicicleta que vuela",
+        precio: 5800,
+        talle: "56",
+        img: './public/images/specialized-aethos-comp-2022.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Neopreno Orca Trn Core hombre",
+        tipo: "neopreno",
+        desc: "Muy buena flotabilidad y flexibilidad",
+        precio: 169,
+        talle: "L",
+        img: './public/images/neopreno-orca-trn-core-hombre-2021.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Bielas Shimano Ultegra R8000",
+        tipo: "transmisiones",
+        desc: "Brazos de biela ligeros",
+        precio: 300,
+        talle: "11V",
+        img: './public/images/bielas-shimano-ultegra-r-v.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Zapatillas Speed Goat 3",
+        tipo: "zapatillas",
+        desc: "Zapatillas para ritmos bajos",
+        precio: 140,
+        talle: "42",
+        img: './public/images/zapatillas-hoka-speedgoat-3-.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Scott Addict RC Pro",
+        tipo: "bicicleta",
+        desc: "Una bicicleta para llano o montaña",
+        precio: 9099,
+        talle: "56",
+        img: './public/images/scott-addict-rc-pro-.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Neopreno Orca Trn Mujer",
+        tipo: "neopreno",
+        desc: "Muy buena flotabilidad y flexibilidad",
+        precio: 169,
+        talle: "L",
+        img: './public/images/neopreno-orca-openwater-trn-mujer.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
